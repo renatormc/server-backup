db97188ec0bde1e8ba144491c47c5d36b5dbba15
renatormc <renatomartinsrmc@gmail.com>
2023-02-23 14:52:10 -0300

    normal

--- build.py
+++ build.py
@@ -5,7 +5,8 @@ import sys
 
 current_dir = Path(".")
 exe_path = Path("./dist/server-backup")
-subprocess.check_call(['go1.20', "build", "-o", str(exe_path)])
+goexe = input("go command (ex: go, go1.20): ")
+subprocess.check_call([goexe, "build", "-o", str(exe_path)])
 
 if len(sys.argv) > 1:
     subprocess.check_call([str(exe_path)] + sys.argv[1:])
